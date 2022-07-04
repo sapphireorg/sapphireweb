e8458a1ea5d69c2afaea46cf1d73b3e07121d552
Panda <90091858+PandaSLO@users.noreply.github.com>
2022-07-04 16:02:59 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Sapphire',
-  tagline: 'Specialized discord bot',
+  tagline: 'Specialized discord bot.',
   url: 'https://sapphiredev.tk/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
